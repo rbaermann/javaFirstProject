a7f79bfba22ff9c0d78c9f6d2caaede3eab38898
Ryan Baermann <rcbaermann@gmail.com>
2019-09-30 10:57:53 -0700

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        int age = 23;
+        String name = "Ryan";
+        String hometown = "Monroe, WA";
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
